c7d7cb01e7ea2028b626bbc2054f689bb09b3db0
Salim A. JOLY <el.salim.salim@gmail.com>
2021-12-28 16:22:36 +0100

    ok

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"],
+    val: 6
+  }
   constructor(props) {
     super(props);
 
@@ -13,15 +17,11 @@ class Die extends Component {
   }
 
   render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
+    const {numberWords, locked, val, disabled, rolling} = this.props;
+    let diceIcon = "Die fas fa-dice-" + numberWords[val - 1] + " fa-5x";
+    if (locked) diceIcon += " Die-locked";
+    if (rolling) diceIcon += " Die-rolling";
+    return <i className={diceIcon} onClick={this.handleClick} ></i>;
   }
 }
 
